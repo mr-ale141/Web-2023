15e528ebaef9637c1528763fa849a36565bc0b0c
mr-ale141 <ale141@rambler.ru>
2023-04-26 17:07:58 +0300

    lw8 Start admin/login

---
+++ static/js/admin-login.js
@@ -0,0 +1,120 @@
+const mainForm = document.forms.main;
+
+const mainFormTitle = document.querySelector('#formTitle');
+const mainFormUserEmail = mainForm.userEmail;
+const mainFormUserPass = mainForm.userPass;
+const mainFormToggleIcon = document.getElementsByClassName('main-form__toggle-icon')[0];
+
+console.log(mainFormToggleIcon.src);
+
+mainFormToggleIcon.addEventListener("click", function (event) {
+  if (mainFormToggleIcon.src === "../static/img/admin/eye-off.png") {
+    mainFormToggleIcon.src = "../static/img/admin/eye.png"
+  } else {
+    mainFormToggleIcon.src="../static/img/admin/eye-off.png";
+  }
+});
+
+mainFormUserEmail.addEventListener("focus", function (event) {
+  mainFormUserEmail.classList.add("main-form__item-input_completed");
+});
+
+mainFormUserEmail.addEventListener("blur", function (event) {
+  if (mainFormUserEmail.value === "") {
+    mainFormUserEmail.classList.remove("main-form__item-input_completed");
+  }
+});
+
+mainFormUserPass.addEventListener("focus", function (event) {
+  mainFormUserPass.classList.add("main-form__item-input_completed");
+});
+
+mainFormUserPass.addEventListener("blur", function (event) {
+  if (mainFormUserPass.value === "") {
+    mainFormUserPass.classList.remove("main-form__item-input_completed");
+  }
+});
+
+mainForm.addEventListener("submit", function (event) {
+  console.log(document.getElementsByClassName("main-form__password_hide").nextElementSibling);
+
+  if (mainFormUserEmail.value) {
+    if (mainFormUserEmail.nextElementSibling === document.getElementsByClassName("main-form__empty-email")[0]) {
+      mainFormUserEmail.nextElementSibling.remove();
+    }
+    mainFormUserEmail.classList.remove("main-form__item-input_invalid");
+  } else {
+    if (mainFormUserEmail.nextElementSibling) {
+      mainFormUserEmail.nextElementSibling.remove();
+    }
+    mainFormUserEmail.insertAdjacentHTML(
+      "afterend",
+      `<div class="main-form__empty-email">
+          Email is required.
+        </div>`
+    );
+    mainFormUserEmail.classList.add("main-form__item-input_invalid");
+  }
+
+  if (mainFormUserPass.value) {
+    if (mainFormUserPass.nextElementSibling === document.getElementsByClassName("main-form__empty-pass")[0]) {
+      mainFormUserPass.nextElementSibling.remove();
+    }
+    mainFormUserPass.classList.remove("main-form__item-input_invalid");
+  } else {
+    if (mainFormUserPass.nextElementSibling) {
+      mainFormUserPass.nextElementSibling.remove();
+    }
+    mainFormUserPass.insertAdjacentHTML(
+      "afterend",
+      `<div class="main-form__empty-pass">
+          Password is required.
+        </div>`
+    );
+    mainFormUserPass.classList.add("main-form__item-input_invalid");
+  }
+  
+  if (!mainFormUserEmail.value || !mainFormUserPass.value) {
+    if (mainFormTitle.nextElementSibling === mainFormUserEmail.parentElement) {
+      mainFormTitle.insertAdjacentHTML(
+        "afterend",
+        `<div class="main-form__empty-error">
+          A-Ah! Check all fields,
+        </div>`
+      );
+    }
+  } else {
+    if (mainFormTitle.nextElementSibling === document.getElementsByClassName("main-form__empty-error")) {
+      mainFormTitle.nextElementSibling.remove();
+    }
+  }
+
+  if (emailTest(mainFormUserEmail)) {
+    if (!mainFormUserEmail.nextElementSibling) {
+      mainFormUserEmail.insertAdjacentHTML(
+        "afterend",
+        `<div class="main-form__email-error">
+          Incorrect email format. Correct format is ****@**.***
+        </div>`
+      );
+      mainFormUserEmail.classList.add("main-form__item-input_invalid");
+    }
+    event.preventDefault();
+  } else {
+    if (mainFormUserEmail.nextElementSibling === document.getElementsByClassName("main-form__email-error")[0]) {
+      mainFormUserEmail.nextElementSibling.remove();
+    }
+  }
+
+  if (passTest(mainFormUserPass)) {
+    event.preventDefault();
+  }
+});
+
+function emailTest(input) {
+  return !/\S+@\S+\.\S+/.test(input.value);
+}
+
+function passTest(input) {
+  return !/[A-Za-z0-9\s]{1,}/.test(input.value);
+}
