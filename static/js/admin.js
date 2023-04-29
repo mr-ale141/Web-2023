4293d7a722f86453371e21c33f0a2cbdb92fca83
mr-ale141 <ale141@rambler.ru>
2023-04-30 00:17:47 +0300

    admin/create css end

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,1 +1,25 @@
-pattern = "[0-9]{2}\.[0-9]{2}\.[0-9]{4}"
+//pattern = "[0-9]{2}\.[0-9]{2}\.[0-9]{4}"
+
+const inputDate = document.getElementById('date');
+
+console.log(inputDate);
+
+const options = {
+  weekday: 'numeric',
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric'
+};
+
+let now = new Date();
+console.log(now.toLocaleDateString('en-US'));
+
+nowString = new Intl.DateTimeFormat('en-US').format(now);
+
+console.log(nowString);
+
+inputDate.valueAsDate = now;
+
+
+console.log(inputDate.valueAsDate);
+
