2d4ee3740870bc1ac258e0725f3a215465a47a5e
mr-ale141 <ale141@rambler.ru>
2023-05-21 16:04:26 +0300

    lw11 lw12 end

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,27 @@ window.addEventListener('load', function () {
   
   addHandlerLogIn(buttonAdmin);
 
+  const burgerHeader = document.querySelectorAll(".menu__burger")[0];
+  const burgerFooter = document.querySelectorAll(".menu__burger")[1];
+
+  burgerHeader.addEventListener("click", (e) => {
+    const blockLinkHeader = document.querySelectorAll(".menu__block-link")[0];
+    if (blockLinkHeader.classList.contains("menu__block-link_show")) {
+      blockLinkHeader.classList.remove("menu__block-link_show");
+    } else {
+      blockLinkHeader.classList.add("menu__block-link_show");
+    }
+  });
+  
+  burgerFooter.addEventListener("click", (e) => {
+    const blockLinkFooter = document.querySelectorAll(".menu__block-link")[1];
+    if (blockLinkFooter.classList.contains("menu__block-link_show")) {
+      blockLinkFooter.classList.remove("menu__block-link_show");
+    } else {
+      blockLinkFooter.classList.add("menu__block-link_show");
+    }
+  });
+
   function addHandlerLogIn(inputs) {
     for (let index = 0; index < inputs.length; index++) {
       inputs[index].addEventListener("click", async function (event) {
